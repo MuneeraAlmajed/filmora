930029dadbb3e953ac97b3ea9f3d1198e7c7fb4b
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-08-23 10:21:29 +0300

    css in progress

--- controllers/movieCtrl.js
+++ controllers/movieCtrl.js
@@ -22,6 +22,66 @@ const newMovie = async(req,res)=>{
 
 }
 
+const createMovie = async(req,res)=>{
+    try{
+        const movie = await Movie.create(req.body);
+        
+        res.redirect('/movies');
+    }catch(err){
+        console.log(err)
+        res.render('/movies/new')
+    }
+}
+
+const showMovie = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.movieId);
+
+    res.render('movies/show.ejs', { movie });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/movies');
+  }
+};
+
+const editMovie = async(req,res)=>{
+    try{
+        const movie = await Movie.findById(req.params.movieId);
+        res.render('movies/edit.ejs', {movie});
+    
+    }catch(err){
+        console.log(err)
+        res.redirect('/');
+    }
+}
+
+const updateMovie = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.movieId);
+
+    movie.set(req.body);
+
+    await movie.save();
+
+    res.redirect(`/movies/${movie._id}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect('/movies');
+  }
+};
+
+const deleteMovie = async(req,res)=>{
+    try{
+       const movie = await Movie.findByIdAndDelete(req.params.movieId);
+       await movie.save();
+       res.redirect(`/movies/${movie._id}`);
+
+    }catch(err){
+        console.log(err)
+        res.redirect('/movies')
+    }
+}
+
 module.exports = {
-    index, newMovie,
+    index, newMovie, createMovie, showMovie, editMovie, updateMovie, deleteMovie,
 }
